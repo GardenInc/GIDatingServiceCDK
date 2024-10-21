08c3632dc8539dd4bdb4e94751bace72a0770f0e
Michael Obrien <michaelobrien@Michaels-MacBook-Pro.local>
2024-10-21 16:22:44 -0700

    Initial commit

---
+++ test/gi_dating_service_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GiDatingServiceCdk from '../lib/gi_dating_service_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gi_dating_service_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GiDatingServiceCdk.GiDatingServiceCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
