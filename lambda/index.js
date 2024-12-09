a87952c0da468188ade189aebf9e35594ea52925
Michael Obrien <michaelobrien@mac.lan>
2024-12-09 11:00:40 -0500

    fix

--- lambda/index.js
+++ lambda/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: `Hello from the ${process.env.STAGE_NAME} environment!\n`,
-    };
-    return response;
+  const response = {
+    statusCode: 200,
+    body: `Hello from the ${process.env.STAGE_NAME} environment!\n`,
+  };
+  return response;
 };
