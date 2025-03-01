65eefd4507c91342ef30d9e7f813ca9a15db3452
Michael Obrien <michaelobrien@Michaels-MacBook-Pro.local>
2025-03-01 11:36:26 -0600

    again

--- src/lib/stacks/frontend/frontendPipelineStack.ts
+++ src/lib/stacks/frontend/frontendPipelineStack.ts
@@ -6,7 +6,13 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as kms from 'aws-cdk-lib/aws-kms';
 import { App, Stack, StackProps, RemovalPolicy, CfnOutput, CfnCapabilities, SecretValue } from 'aws-cdk-lib';
 import { FrontEndStackConfigInterface } from '../../utils/config';
-import { SECRET_NAME, FrontendPipelineStackName, TEMPLATE_ENDING, DEVICE_FARM_STACK } from '../../utils/constants';
+import { 
+  SECRET_NAME, 
+  FrontendPipelineStackName, 
+  TEMPLATE_ENDING, 
+  DEVICE_FARM_STACK,
+  DEPLOYMENT_BUCKET_STACK,
+} from '../../utils/constants';
 import { pipelineAccountId } from '../../utils/accounts';
 import { Duration } from 'aws-cdk-lib';
 
@@ -113,7 +119,11 @@ export class FrontendPipelineStack extends Stack {
         },
         artifacts: {
           'base-directory': 'dist',
-          files: [`${FrontendPipelineStackName}${TEMPLATE_ENDING}`, `*${DEVICE_FARM_STACK}${TEMPLATE_ENDING}`],
+          files: [
+            `${FrontendPipelineStackName}${TEMPLATE_ENDING}`, 
+            `*${DEVICE_FARM_STACK}${TEMPLATE_ENDING}`,
+            `*${DEPLOYMENT_BUCKET_STACK}${TEMPLATE_ENDING}`,
+          ],
         },
       }),
       environment: {
